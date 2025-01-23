a1fa31286d5f343ca789fb3a8f792f78b9064663
Praveen Singh <praveen969500@gmail.com>
2025-01-23 19:24:20 +0530

    make route /vedioUpload and make a handler to upload a vedio on cloudinary and store data in database

--- controller/fileupload_controller.js
+++ controller/fileupload_controller.js
@@ -37,6 +37,7 @@ function isFileTypeSupported(fileType, supportedTypes) {
 
 async function uploadFileToClodinary(file, folder) {
     const options = { folder };
+    options.resource_type = "auto";
     return await cloudinary.uploader.upload(file.tempFilePath, options);
 }
 
@@ -48,7 +49,7 @@ exports.imageUpload = async (req, res) => {
         console.log(name, tags, email);
 
         //fetch file from the request 
-        const file = req.files.file;
+        const file = req.files.imageFile;
         console.log(file);
 
         //validation 
@@ -86,6 +87,67 @@ exports.imageUpload = async (req, res) => {
             success: false,
             message: `Something went wrong67`,
         });
+
+    }
+}
+
+// vedio upload handler
+exports.vedioUpload = async (req, res) => {
+    try {
+        const { name, tags, email } = req.body;
+        console.log(name, tags, email);
+        //fetch file from request
+        const file = req.files.vedioFile;
+        console.log(file);
+
+        //supported type 
+        const supportedTypes = ["mp4", "mov"];
+        const fileType = file.name.split('.')[1].toLowerCase();
+
+        //validation
+
+        // if formate of vedio file is not more than 5 mb
+        if (file.size >= 5000000) {
+            return res.status(400).json({
+                success: false,
+                message: `File size is exceeded 5MB`,
+            })
+        }
+
+        // if formate of vedio file is not mp4 or mov 
+        if (!isFileTypeSupported(fileType, supportedTypes)) {
+            return res.status(400).json({
+                success: false,
+                message: `File format not supported`,
+            })
+        }
+
+        // if file formate is supported then 
+        console.log("before the upload file");
+        const response = await uploadFileToClodinary(file, "EdTech"); // edtech is folder name where we create  on cloundinary
+        console.log(response);
+
+        //save data in database
+        const fileData = await File.create({
+            name,
+            tags,
+            email,
+            imageUrl: response.secure_url,
+
+        });
+        res.status(200).json({
+            success: true,
+            imageUrl: response.secure_url,
+            message: 'vedio uploaded successfully',
+        })
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            success: false,
+            message: 'Something went wrong67',
+        });
+
 
     }
 }
--- routes/fileupload_route.js
+++ routes/fileupload_route.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const { localFileUpload, imageUpload } = require("../controller/fileupload_controller");
+const { localFileUpload, imageUpload, vedioUpload } = require("../controller/fileupload_controller");
 //api route
 router.post("/localFileUpload", localFileUpload);
 router.post("/imageUpload", imageUpload);
+router.post("/vedioUpload", vedioUpload);
 module.exports = router;
